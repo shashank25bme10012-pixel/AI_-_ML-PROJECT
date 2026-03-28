8dcdf09ba8ca955ea7a9292486e6f3339a2f5604
shashank25bme10012-pixel <shashank.25bme10012@vitbhopal.ac.in>
2026-03-28 20:53:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ class MazeSolver:
                 elif self.maze[r][c] == 'E':
                     end = (r, c)
         return start, end
+
+def is_valid(self, row, col):
+        if 0 <= row < self.rows and 0 <= col < self.cols:
+            if self.maze[row][col] != 1 and (row, col) not in self.visited:
+                return True
+        return False
+
+    def get_neighbors(self, row, col):
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        neighbors = []
+        for dr, dc in directions:
+            neighbors.append((row + dr, col + dc))
+        return neighbors
