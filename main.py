a37881c5c6efb95ca4dc9f5068777271d392d828
shashank25bme10012-pixel <shashank.25bme10012@vitbhopal.ac.in>
2026-03-29 10:26:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,26 @@ def is_valid(self, row, col):
         for dr, dc in directions:
             neighbors.append((row + dr, col + dc))
         return neighbors
+def solve(self):
+        start, end = self.get_start_end()
+        if not start or not end:
+            return None
+
+        stack = [(start, [start])]
+        
+        while stack:
+            (curr_row, curr_col), current_path = stack.pop()
+            
+            if (curr_row, curr_col) == end:
+                self.path = current_path
+                return self.path
+            
+            if (curr_row, curr_col) not in self.visited:
+                self.visited.add((curr_row, curr_col))
+                
+                for next_row, next_col in self.get_neighbors(curr_row, curr_col):
+                    if self.is_valid(next_row, next_col):
+                        new_path = list(current_path)
+                        new_path.append((next_row, next_col))
+                        stack.append(((next_row, next_col), new_path))
+        return None
